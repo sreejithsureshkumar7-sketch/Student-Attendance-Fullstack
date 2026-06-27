65556367d4c82211729d5edd3cf1ad51f3d111aa
Sreejith S <sreejithsureshkumar7@gmail.com>
2026-06-27 20:43:49 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,1 +1,6 @@
-import express from "express";import {register,login} from "../controllers/authController.js";const r=express.Router();r.post("/register",register);r.post("/login",login);export default r;
+import express from "express";
+import {register,login} from "../controllers/authController.js";
+const r=express.Router();
+r.post("/register",register);
+r.post("/login",login);
+export default r;
